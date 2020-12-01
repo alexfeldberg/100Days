271eaa0e00010bb5127b29b640e430429c7ace70
Alex Feldberg <alexfeldberg95@gmail.com>
2020-12-01 16:55:24 -0500

    removing old folders

--- day1/bandnamegen.py
+++
@@ -1,8 +0,0 @@
-def getbandname():
-	print("Welcome to the Band Name Generator.")
-	city = input("What's the name of the city you grew up in?\n")
-	pet_name = input("What's the name of your pet?\n")
-
-	print("Your band name could be: " + city + " " + pet_name)
-
-getbandname()
