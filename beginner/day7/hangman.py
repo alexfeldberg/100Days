45228c05e042db519ffda875539ae92141b86a87
Alex Feldberg <alexfeldberg95@gmail.com>
2020-12-01 17:36:54 -0500

    Added files for more words, art. Added functionality for correct and incorrect guesses/not removing lives for incorrect guesses that were already made.

--- beginner/day7/hangman.py
+++ beginner/day7/hangman.py
@@ -1,92 +1,46 @@
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+from hangman_art import logo, stages
+from hangman_words import word_list
 
 def hangman():
 
-	word_list = ["ardvark", "baboon", "camel"]
+	print(logo)
 	end_of_game = False
+	lives = len(stages) - 1 #one life per body part
+	#Choose random word from hangman_words.py
 	chosen_word = random.choice(word_list)
-	# print(f'Pssst, the solution is {chosen_word}.')
+	print(f'Pssst, the solution is {chosen_word}.') #used for testing
 
-	lives = 6 #one life per body part
-
+	#added extra functionality to not remove a life for a wrong guess that was already guessed
+	wrong_guesses = set([])
+	
 	#Creating blanks
 	display = []
 	for _ in chosen_word:
 		display.append("_")
+	#Print to show length of word
 	print(display)
 
 	while not end_of_game:
 		guess = input("Guess a letter: ").lower()
 
+		if guess in display:
+			print(f"You've already guessed {guess}, it is in the word. Try again.")
 		#Check guessed letter
 		for pos in range(len(chosen_word)):
 			if chosen_word[pos] == guess:
 				display[pos] = guess
 
 		if guess not in chosen_word:
-			lives -= 1
-			print(f"Oh no! That letter is not in the word, you lose a life! You have {lives} lives left.")
-			print(stages[lives])
-			if lives == 0:
-				end_of_game = True
-				print(f"Sorry, you lost! The word was: {chosen_word}")
+			if guess in wrong_guesses:
+				print(f"You've already guessed {guess}, it is not in the word. Try again.")
+			else:
+				wrong_guesses.add(guess)
+				lives -= 1
+				print(f"Oh no! That letter is not in the word, you lose a life! You have {lives} lives left.")
+				if lives == 0:
+					end_of_game = True
+					print(f"Sorry, you lost! The word was: {chosen_word}")
 
 
 		print(display)
@@ -94,6 +48,9 @@ def hangman():
 		if "_" not in display:
 			end_of_game = True
 			print("Congratulations! You won!")
+
+		#print hangman regardless of if guess is correct
+		print(stages[lives])
 
 
 hangman()
