75a91644287b021a817dc815da2789ea4e4ca240
Alex Feldberg <alexfeldberg95@gmail.com>
2020-12-09 16:19:40 -0500

    Day 17 project - Quiz (OOP)

---
+++ intermediate/day17/data.py
@@ -0,0 +1,12 @@
+question_data = [
+	{"text":"'Typewriter' is the longest word that can be typed using only the first row on a QWERTY keyboard.","answer":"True"},
+	{"text":"Coca-Cola's original colour was green.","answer":"False"},
+	{"text":"Cucumbers are usually more than 90% water.","answer":"True"},
+	{"text":"The French word for travel is 'Travail'","answer":"False"},
+	{"text":" Einstein had trouble with mathematics when he was in school.","answer":"False"},
+	{"text":"'Santa Claus' is a variety of melon.","answer":"True"},
+	{"text":"The scientific name for the Southern Lights is Aurora Australis?","answer":"True"},
+	{"text":"Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing a chicken with snow.","answer":"True"},
+	{"text":"The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.","answer":"False"},
+	{"text":"'Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.' is a grammatically correct sentence.","answer":"True"}
+	]
