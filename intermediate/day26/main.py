0e59253896b23491a99976266bd6d39f8577c5f8
Alex Feldberg <alexfeldberg95@gmail.com>
2021-01-14 17:23:31 -0500

    Added error handling

--- intermediate/day26/main.py
+++ intermediate/day26/main.py
@@ -7,6 +7,15 @@ nato_alphabet = {row.letter:row.code for (index, row) in data.iterrows()}
 # print(result)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
-nato = (nato_alphabet[letter] for letter in word)
-print(nato)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        nato = (nato_alphabet[letter] for letter in word)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        generate_phonetic()
+    else:
+        print(nato)
+
+
+generate_phonetic()
